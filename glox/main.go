eeb63f2e4866b647d082fc2f8ab964e4735034bf
Tim Millard <tim@rapidpaylegal.com>
2023-04-28 15:25:16 +1000

    Start up

--- glox/main.go
+++ glox/main.go
@@ -1,6 +1,30 @@
 package main
 
-func main() {
+import (
+"os"
+"fmt"
+)
 
+func main() {
+	
+	if len(os.Args) < 2 {
+		runPrompt();
+	}
+	if len(os.Args) == 2 {
+		runFile(os.Args[1])
+	}
+	if len(os.Args) > 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s [script]\n", os.Args[0])
+		os.Exit(64)
+	}
 }
+
+func runFile(filename string) {
+	fmt.Println("runFile", filename)
+}
+
+func runPrompt() {
+	fmt.Println("runPrompt")
+}
+
 
